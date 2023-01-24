113b3e6f9b773f4ee35dce31fef5e3fb74ec9dc2
Victor Bahta <120078818+victorbahtas@users.noreply.github.com>
2023-01-24 15:43:10 -0600

    Update Member.java

--- Models/Member.java
+++ Models/Member.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.util.List;
 
 public class Member extends Person implements Serializable {
+    public Member(String fname, String lname, Address address, Account acct) {
+        super(fname,lname,address,acct);
+    }
     private String memberId;
     private  List<CheckoutRecord> checkOutRecord;
 }
+
