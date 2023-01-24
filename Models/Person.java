c1709b0954f3f5e31d2afdf9f5f278c2b5ec205d
Victor Bahta <120078818+victorbahtas@users.noreply.github.com>
2023-01-24 15:42:25 -0600

    Update Person.java

--- Models/Person.java
+++ Models/Person.java
@@ -1,3 +1,5 @@
+
+
 package Models;
 
 public class Person {
@@ -5,4 +7,43 @@ public class Person {
     private String lastName;
     private Address address;
     private Account account;
+
+    public Person(String firstName, String lastName, Address address, Account account) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.account = account;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 }
