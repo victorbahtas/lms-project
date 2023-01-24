4c987a74f6b80e07bc7e147ed7c3e12057e38d45
Victor Bahta <120078818+victorbahtas@users.noreply.github.com>
2023-01-24 15:31:34 -0600

    Update Admin.java

--- Models/Admin.java
+++ Models/Admin.java
@@ -1,6 +1,22 @@
 package Models;
 
-public class Admin extends Person{
-  public String adminId;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
+public class Admin {
+    public static void addNewMember(Member m) {
+        try {
+            FileOutputStream file = new FileOutputStream("Members.data");
+            ObjectOutputStream output = new ObjectOutputStream(file);
+            output.writeObject(m);
+            System.out.println("Object saved in data");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
