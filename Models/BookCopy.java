dc2d81e278d2055ba8b95a9bb03630f073283a67
muazkubur95 <123494153+muazkubur95@users.noreply.github.com>
2023-01-24 16:02:18 -0600

    Update BookCopy.java

--- Models/BookCopy.java
+++ Models/BookCopy.java
@@ -3,4 +3,23 @@ package Models;
 public class BookCopy {
     private int copyNum;
     private Book book;
+    public Book getBook(){
+        return book;
+    }
+
+    public int getCopyNum() {
+        return copyNum;
+    }
+
+    public void setCopyNum(int copyNum) {
+        this.copyNum = copyNum;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getTitle() {
+        return book.getTitle();
+    }
 }
