2e130f9a33d1aa6158199f8e664d418bc4144c34
muazkubur95 <123494153+muazkubur95@users.noreply.github.com>
2023-01-24 16:01:25 -0600

    Update Book.java

--- Models/Book.java
+++ Models/Book.java
@@ -8,10 +8,64 @@ public class Book {
     private int maxCheckLength;
     private List<BookCopy> bookCopyList;
     private List<Author> authorList;
-    
-    
-    public boolean isAvailable(){
+    public Book(String title, String isbn){
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getMaxCheckLength() {
+        return maxCheckLength;
+    }
+
+    public void setMaxCheckLength(int maxCheckLength) {
+        this.maxCheckLength = maxCheckLength;
+    }
+
+    public List<BookCopy> getBookCopyList() {
+        return bookCopyList;
+    }
+
+    public void setBookCopyList(List<BookCopy> bookCopyList) {
+        this.bookCopyList = bookCopyList;
+    }
+
+    public List<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void setAuthorList(List<Author> authorList) {
+        this.authorList = authorList;
+    }
+    private Book searchBook(String title) {
+        for (BookCopy book : bookCopyList) {
+            if (book.getTitle().equals(title)) {
+                return book.getBook();
+            }
+        }
+        return null;
+    }
+        public boolean isAvailable(){
            return bookCopyList.size()==0? return false: true;
     }
+}
+
+    
+
  
 }
