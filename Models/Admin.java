e19e44587fbaa6dbbb4104074d33c1f04ca77d54
Victor Bahta <120078818+victorbahtas@users.noreply.github.com>
2023-01-24 15:52:22 -0600

    Update Admin.java

--- Models/Admin.java
+++ Models/Admin.java
@@ -5,13 +5,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-public class Admin {
-    public static void addLibraryMember(Member m) {
+public class Admin extends Person {
+    public Admin(String firstName, String lastName, Address address, Account account) {
+        super(firstName, lastName, address, account);
+    }
+
+    public static void addNewMember(Member m) {
         try {
             FileOutputStream file = new FileOutputStream("Members.data");
             ObjectOutputStream output = new ObjectOutputStream(file);
             output.writeObject(m);
-            System.out.println("Member saved in File");
+            System.out.println("Object saved in data");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
